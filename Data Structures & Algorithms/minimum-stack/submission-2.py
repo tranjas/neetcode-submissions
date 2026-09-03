c9243893f0de717677f88987349dabaceb099bfe
Jason Tran <125832558+tranjas@users.noreply.github.com>
2026-09-03 00:33:05 -0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.m_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.m_stack and self.m_stack[-1][1] < val:
+            self.m_stack.append((val, self.m_stack[-1][1]))
+        else:
+           self.m_stack.append((val, val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.m_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.m_stack[-1][1]
